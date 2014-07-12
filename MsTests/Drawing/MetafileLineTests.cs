64d7c2ccfec7d6b22586c0e6295d1fcae5702fd8
Alena <alenasv70@gmail.com>
2014-07-12 15:44:53 +0300

    tests adjusted

---
+++ MsTests/Drawing/MetafileLineTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MsTests.Drawing
+{
+    class MetafileLineTests
+    {
+        private String pathMetafiles = Utils.METAFILES_PATH + Utils.LINES_SPATH + Utils.CS_MF_SPATH;
+        private String pathPng = Utils.METAFILES_PATH + Utils.LINES_SPATH + Utils.CS_PNG_SPATH;
+
+
+        [Test]
+        public void TestDrawLine10EMF()
+        {
+            String imageName = "MetafileLine10";
+            String metafileName = imageName + Utils.CS_EMF_EXT;
+            String pngName = imageName + Utils.CS_EMF_PNG_EXT;
+            String imagePath = Path.Combine(pathMetafiles, metafileName);
+            Graphics gr = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr hdc = gr.GetHdc();
+            using (Metafile metafile = new Metafile(imagePath, hdc))
+            {
+                Graphics g = Graphics.FromImage(metafile);
+                g.DrawLine(new Pen(Color.Red,10), 100,100,300,300);
+                gr.ReleaseHdc(hdc);
+            }
+            Image image = new Metafile(imagePath);
+            image.Save(Path.Combine(pathPng, pngName), ImageFormat.Png);
+        }
+
+ 
+    [Test]
+        public void TestDrawLine30EMF()
+        {
+            String imageName = "MetafileLine30";
+            String metafileName = imageName + Utils.CS_EMF_EXT;
+            String pngName = imageName + Utils.CS_EMF_PNG_EXT;
+            String imagePath = Path.Combine(pathMetafiles, metafileName);
+            Graphics gr = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr hdc = gr.GetHdc();
+            using (Metafile metafile = new Metafile(imagePath, hdc))
+            {
+                Graphics g = Graphics.FromImage(metafile);
+                g.DrawLine(new Pen(Color.Red,10), 100,100,300,300);
+                gr.ReleaseHdc(hdc);
+            }
+            Image image = new Metafile(imagePath);
+            image.Save(Path.Combine(pathPng, pngName), ImageFormat.Png);
+        }
+
+ 
+    [Test]
+        public void TestDrawLine50EMF()
+        {
+            String imageName = "MetafileLine50";
+            String metafileName = imageName + Utils.CS_EMF_EXT;
+            String pngName = imageName + Utils.CS_EMF_PNG_EXT;
+            String imagePath = Path.Combine(pathMetafiles, metafileName);
+            Graphics gr = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr hdc = gr.GetHdc();
+            using (Metafile metafile = new Metafile(imagePath, hdc))
+            {
+                Graphics g = Graphics.FromImage(metafile);
+                g.DrawLine(new Pen(Color.Red,10), 100,100,300,300);
+                gr.ReleaseHdc(hdc);
+            }
+            Image image = new Metafile(imagePath);
+            image.Save(Path.Combine(pathPng, pngName), ImageFormat.Png);
+        }
+
+   [Test]
+        public void TestDrawLine10WMF()
+        {
+            String imageName = "MetafileLine10";
+            String metafileName = imageName + Utils.CS_WMF_EXT;
+            String pngName = imageName + Utils.CS_WMF_PNG_EXT;
+            String imagePath = Path.Combine(pathMetafiles, metafileName);
+            Graphics gr = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr hdc = gr.GetHdc();
+            using (Metafile metafile = new Metafile(imagePath, hdc))
+            {
+                Graphics g = Graphics.FromImage(metafile);
+                g.DrawLine(new Pen(Color.Red,10), 100,100,300,300);
+                gr.ReleaseHdc(hdc);
+            }
+            Image image = new Metafile(imagePath);
+            image.Save(Path.Combine(pathPng, pngName), ImageFormat.Png);
+        }
+
+ 
+    [Test]
+        public void TestDrawLine30WMF()
+        {
+            String imageName = "MetafileLine30";
+            String metafileName = imageName + Utils.CS_WMF_EXT;
+            String pngName = imageName + Utils.CS_WMF_PNG_EXT;
+            String imagePath = Path.Combine(pathMetafiles, metafileName);
+            Graphics gr = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr hdc = gr.GetHdc();
+            using (Metafile metafile = new Metafile(imagePath, hdc))
+            {
+                Graphics g = Graphics.FromImage(metafile);
+                g.DrawLine(new Pen(Color.Red,10), 100,100,300,300);
+                gr.ReleaseHdc(hdc);
+            }
+            Image image = new Metafile(imagePath);
+            image.Save(Path.Combine(pathPng, pngName), ImageFormat.Png);
+        }
+
+ 
+    [Test]
+        public void TestDrawLine50WMF()
+        {
+            String imageName = "MetafileLine50";
+            String metafileName = imageName + Utils.CS_WMF_EXT;
+            String pngName = imageName + Utils.CS_WMF_PNG_EXT;
+            String imagePath = Path.Combine(pathMetafiles, metafileName);
+            Graphics gr = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr hdc = gr.GetHdc();
+            using (Metafile metafile = new Metafile(imagePath, hdc))
+            {
+                Graphics g = Graphics.FromImage(metafile);
+                g.DrawLine(new Pen(Color.Red,10), 100,100,300,300);
+                gr.ReleaseHdc(hdc);
+            }
+            Image image = new Metafile(imagePath);
+            image.Save(Path.Combine(pathPng, pngName), ImageFormat.Png);
+        }
+
+ 
+    }
+}
